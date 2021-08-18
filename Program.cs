171cbb494bea24b23fb5a77203dd6023430409b2
Rafaero <rafaelpaivavitor@hotmail.com>
2021-08-18 11:22:01 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Adicionar um aluno");
+            Console.WriteLine("2- Remover um aluno");
+            Console.WriteLine("3- Calcular média");
+            Console.WriteLine("X - Sair");
+
+            string opcaoUsuario = Console.ReadLine(); 
+
+            while(opcaoUsuario.ToUpper() != "X"){
+                
+                switch(opcaoUsuario){
+
+                    case "1":
+                    break;
+
+                    case "2":
+                    break;
+
+                    case "3":
+                    break;
+
+                    default:
+                    throw new ArgumentOutOfRangeException();
+                }
+
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Adicionar um aluno");
+            Console.WriteLine("2- Remover um aluno");
+            Console.WriteLine("3- Calcular média");
+            Console.WriteLine("X - Sair");
+
+            opcaoUsuario = Console.ReadLine(); 
+            }
+        }
+    }
+}
